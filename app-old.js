93711abe3582a0ca8582df252bed64de8257c25f
Víctor Comino <vcomino@re7systems.com>
2023-07-21 10:18:04 +0200

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.write('Hola Mundo');
+    response.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto', 8080);
